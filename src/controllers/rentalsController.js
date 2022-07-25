f164e305d7bfb251862e2b1a1f1a340851842b11
CaioVitor1 <caiovitor33@hotmail.com>
2022-07-24 23:03:15 -0300

    finalizing post and delete in rentals routes

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -5,14 +5,15 @@ import dayjs from "dayjs";
 
 export async function listRentals(req,res) {
     try{
-        const {rows: rentals} = await connection.query(`
-        SELECT rentals.*, customers.id, customers.name FROM rentals 
-        JOIN customers 
-        ON rentals."customerId" = customers.id;
-        JOIN games
-        `)
-       //apenas um teste, falta complementar essa query de cima
-        res.send(rentals)
+        const query = `SELECT rentals.*, 
+                    json_build_object('id', customers.id, 'name', customers.name) AS customer,
+                    json_build_object('id', games.id, 'name', games.name, "categoryId", games."categoryId", 'categoryName', categories.name) AS game
+                    FROM rentals
+                    JOIN customers ON rentals."customerId" = customers.id
+                    JOIN games ON rentals."gameId" = games.id
+                    JOIN categories ON games."categoryId" = categories.id`;
+        const { rows: rentals } = await connection.query(query);
+        res.send(rentals);
     
     } catch(erro) {
         res.sendStatus(500)
@@ -61,5 +62,69 @@ export async function insertRentals(req, res){
     }catch(erro) {
         console.log(erro);
         res.sendStatus(500);
+    }
+}
+
+export async function returnRentals(req, res) {
+    try{
+        const {id} = req.params;
+        let returnDate = dayjs().format('YYYY-MM-DD');
+     
+        
+        let delayFee = 0;
+        const {rows: rental} = await connection.query('SELECT rentals.*, games."pricePerDay" FROM rentals JOIN games ON rentals.id = $1', [id])
+        
+        if(rental.length === 0) {
+            return res.sendStatus(404)
+        }
+        if(rental[0].returnDate !== null) {
+            return res.sendStatus(400)
+        }
+        const texto = JSON.stringify(rental[0].rentDate);
+
+        const selectDate = texto.substr(1,10)
+        const date1 = dayjs(selectDate)      
+
+        let daysDifference = date1.diff(returnDate, 'day');
+        
+        if(daysDifference > rental[0].daysRented) {
+            let daysLater = daysDifference - rental[0].daysRented
+            console.log(daysLater)
+            delayFee = (daysLater)*(rental[0].pricePerDay)
+        }
+        
+         console.log(returnDate);
+       
+         console.log(delayFee)
+        
+       
+        const updateRentals = await connection.query(`UPDATE rentals SET "returnDate" = '${returnDate}', "delayFee" = ${delayFee}  WHERE id = $1;`, [id])
+        return res.sendStatus(200)
+
+
+    }catch(erro) {
+        console.log(erro)
+        return res.sendStatus(500);
+    }
+}
+
+export async function deleteRentals(req, res) {
+    try{
+        const {id} = req.params;
+        const {rows: searchId} = await connection.query('SELECT * FROM rentals WHERE id = $1', [id]);
+        if(searchId.length === 0) {
+            return res.status(404).send('ID não cadastrado');
+        }
+       
+        if(searchId[0].returnDate === null) {
+            return res.status(400).send('O jogo ainda não foi devolvido');
+        }
+        const {rows: deleteRentals} = await connection.query('DELETE FROM rentals WHERE id = $1;', [id])
+
+        res.sendStatus(200)
+
+    }catch(erro) {
+        console.log(erro)
+        return res.sendStatus(500)
     }
 }
--- src/routes/rentalsRouter.js
+++ src/routes/rentalsRouter.js
@@ -1,8 +1,10 @@
-import { insertRentals, listRentals } from '../controllers/rentalsController.js';
+import { insertRentals, listRentals, returnRentals, deleteRentals } from '../controllers/rentalsController.js';
 import { Router } from 'express';
 
 const router = Router();
 
 router.get('/rentals', listRentals);
-router.post('/rentals', insertRentals)
+router.post('/rentals', insertRentals);
+router.post('/rentals/:id/return', returnRentals);
+router.delete('/rentals/:id', deleteRentals)
 export default router;
